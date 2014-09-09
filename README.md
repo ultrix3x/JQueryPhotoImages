f2a70c88188107423abfd3e5cb303fe7a178d668
Thomas Björk <tb@bytecode.se>
2014-09-09 10:19:44 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,140 @@
-JQueryPhotoImages
-=================
+# JQueryPhotoImages
 
 jQuery-plugin to convert images to photo-like images (frame, shadow and rotation)
+
+## Distribution and development
+There are two different files that can be used.
+
+### resourceloader.js
+is intended for development and testing.
+
+### resourceloader.min.js
+is intended for production usage.
+
+The minification is done by [Googles Closure compiler](http://closure-compiler.appspot.com/) in simple mode.
+
+## Options
+
+### borderWidth
+The width of the border around the image.
+
+**Default value** = 10px
+
+### borderStyle
+The style of the border around the image.
+
+**Default value** = solid
+
+### borderColor
+The color of the border around the image.
+
+**Default value** = #ffffff
+
+### border
+This is a combined value for the border. It work the same way as for the CSS-border value ([width] [style] [color]).
+
+If this value is omitted then it defaults to the properties for borderWidth, borderStyle and borderColor.
+
+If this value is assigned then it overrides the individual properties for  borderWidth, borderStyle and borderColor.
+
+### boxShadowOffsetX
+The x-offset for the box-shadow
+
+**Default value** = 5px
+
+### boxShadowOffsetY
+The y-offset for the box-shadow
+
+**Default value** = 5px
+
+### boxShadowLength
+The shadow length for the box-shadow
+
+**Default value** = 5px
+
+### boxShadowColor
+The color to use as a base for the box-shadow
+
+**Default value** = #cccccc
+
+### boxShadow
+This is a combined value for the box-shadow. It work the same way as for the CSS-box-shadow value ([boxShadowOffsetX], [boxShadowOffsetY], [boxShadowLength], [boxShadowColor]).
+
+If this value is omitted then it defaults to the properties for boxShadowOffsetX, boxShadowOffsetY, boxShadowLength and boxShadowColor.
+
+If this value is assigned then it overrides the individual properties for boxShadowOffsetX, boxShadowOffsetY, boxShadowLength and boxShadowColor.
+
+### marginLeft
+A margin-left to use for the image. If the value is set to "leave" then the margin-left will not be changed.
+
+**Default value** = 'leave
+
+### marginTop
+A margin-top to use for the image. If the value is set to "leave" then the margin-top will not be changed.
+
+**Default value** = 'leave
+
+### marginRight
+A margin-right to use for the image. If the value is set to "leave" then the margin-right will not be changed.
+
+**Default value** = 'leave
+
+### marginBottom
+A margin-bottom to use for the image. If the value is set to "leave" then the margin-bottom will not be changed.
+
+**Default value** = 'leave
+
+### rotate
+If this property is assigned then the image will be rotated.
+
+#### Allowed values
+##### random
+Uses the browsers internal random engine (Math.random) to create an angle to use for each individual image. The min value is defined by the property rotateMin. The max value is defined by the property rotateMax.
+
+##### seed
+Uses the plugins internal random engine to create an angle to use for each individual image. The min value is defined by the property rotateMin. The max value is defined by the property rotateMax.
+
+The big difference bwtween random and seed is that seed will produce the same sequence each time which will allow the same layout for each page load.
+
+##### animate
+Creates an animation. The animation is based on the interval (time between movements) and step (how much to rotate each time).
+
+The interval is defined in microseconds.
+
+The step is defined in degrees.
+
+##### numeric value
+If a numeric value is used then the images is rotated this number of degrees.
+
+##### leave
+If the value is "leave" then nothing is done.
+ 
+**Default value** = leave
+
+### rotateMin
+If the rotate property is set to random or seed then this value defines the lowest angle to rotate.
+
+**Default value** = -20
+
+### rotateMax
+If the rotate property is set to random or seed then this value defines the highest angle to rotate.
+
+**Default value** = 20
+
+### rotateAngle
+This is not a property that can be set. This is calculated as the difference by rotateMax and rotateMin and defines the span of the llowed angle.
+
+### rotateSeed
+When using rotate = "seed" then this defines the seed for the internal "random engine".
+
+**Default value** = 0
+
+### rotateStep
+If using rotate = "animate" then this property defines how many degrees the image should turn each step.
+
+**Default value** = 1
+
+### rotateDelay
+If using rotate = "animate" then this property defines the delay between steps for the animation.
+
+**Default value** = 40
---
+++ jquery.photoimages.js
@@ -0,0 +1,157 @@
+(function(jQuery) {
+  // Make sure that the options object really is an object and has all properties set
+  function Optionize(options) {
+    // Make sure that options is an object
+    options = options || {};
+    // If borderWidth isn't defined then set a default value
+    options.borderWidth = options.borderWidth || '10px';
+    // If borderStyle isn't defined then set a default value
+    options.borderStyle = options.borderStyle || 'solid';
+    // If borderColor isn't defined then set a default value
+    options.borderColor = options.borderColor || '#ffffff';
+    // If border isn't defined then set a default value
+    options.border = options.border || (options.borderWidth + " " + options.borderStyle + " " + options.borderColor);
+    // If boxShadowOffsetX isn't defined then set a default value
+    options.boxShadowOffsetX = options.boxShadowOffsetX || '5px';
+    // If boxShadowOffsetY isn't defined then set a default value
+    options.boxShadowOffsetY = options.boxShadowOffsetY || '5px';
+    // If boxShadowLength isn't defined then set a default value
+    options.boxShadowLength = options.boxShadowLength || '5px';
+    // If boxShadowColor isn't defined then set a default value
+    options.boxShadowColor = options.boxShadowColor || '#cccccc';
+    // If boxShadow isn't defined then set a default value
+    options.boxShadow = options.boxShadow || (options.boxShadowOffsetX + " " + options.boxShadowOffsetY + " " + options.boxShadowLength + " " + options.boxShadowColor);
+    // If marginLeft isn't defined then set a default value
+    options.marginLeft = options.marginLeft || 'leave';
+    // If marginTop isn't defined then set a default value
+    options.marginTop = options.marginTop || 'leave';
+    // If marginRight isn't defined then set a default value
+    options.marginRight = options.marginRight || 'leave';
+    // If marginBottom isn't defined then set a default value
+    options.marginBottom = options.marginBottom || 'leave';
+    // If rotate isn't defined then set a default value
+    options.rotate = options.rotate || 'leave';
+    // If rotateMin isn't defined then set a default value
+    options.rotateMin = options.rotateMin || -20;
+    // If rotateMax isn't defined then set a default value
+    options.rotateMax = options.rotateMax || 20;
+    // If rotateAngle isn't defined then set a default value
+    options.rotateAngle = options.rotateMax - options.rotateMin;
+    // If rotateSeed isn't defined then set a default value
+    options.rotateSeed = options.rotateSeed || 0;
+    // If rotateStep isn't defined then set a default value
+    options.rotateStep = options.rotateStep || 1;
+    // If rotateDelay isn't defined then set a default value
+    options.rotateDelay = options.rotateDelay || 40;
+    return options;
+  }
+  function SetBorder(options, jObject) {
+    // Add the border
+    jObject.css('border', options.border);
+    // Add the box-shadow
+    jObject.css('boxShadow', options.boxShadow);
+  }
+  function SetMargin(options, jObject) {
+    // If marginLeft is defined (not equal to leave) then set it
+    if(options.marginLeft != 'leave') {
+      jObject.css('marginLeft', options.marginLeft);
+    }
+    // If marginTop is defined (not equal to leave) then set it
+    if(options.marginTop != 'leave') {
+      jObject.css('marginTop', options.marginTop);
+    }
+    // If marginRight is defined (not equal to leave) then set it
+    if(options.marginRight != 'leave') {
+      jObject.css('marginRight', options.marginRight);
+    }
+    // If marginBottom is defined (not equal to leave) then set it
+    if(options.marginBottom != 'leave') {
+      jObject.css('marginBottom', options.marginBottom);
+    }
+  }
+  function DoRotateRandom(options, jObject) {
+    // Calculate the randomized angle
+    var rotate = Math.random() * options.rotateAngle;
+    // Add the min value to calculate the actual angle
+    rotate = Math.floor(options.rotateMin + rotate);
+    // Rotate the image
+    Rotate(jObject, rotate);
+  }
+  function DoRotateSeed(options, jObject) {
+    if(options.rotateSeed == 0) {
+      // If seed is 0 then use PI as seed
+      options.rotateSeed = Math.PI;
+    } else if(options.rotateSeed < 0) {
+      // If seed < 0 then recalculate it
+      options.rotateSeed = Math.ln(options.rotateSeed);
+      if(options.rotateSeed != 0) {
+        options.rotateSeed = 1 / Math.abs(options.rotateSeed);
+      }
+    }
+    // Create a new seed value to use
+    options.rotateSeed = (((options.rotateSeed * options.rotateSeed) + Math.PI) * 7) + (1/2);
+    // Make sure the new value is between 0 and 1
+    options.rotateSeed -= Math.floor(options.rotateSeed);
+    // Calculate the angle to use
+    var rotate = options.rotateSeed * options.rotateAngle;
+    // Add the base offset (adds the min value)
+    rotate = Math.floor(options.rotateMin + rotate);
+    // Rotate the image
+    Rotate(jObject, rotate);
+  }
+  // Create an animation
+  function DoRotateAnimate(options, jObject) {
+    // The current angle. This is incremented for each step
+    var rotate = 0;
+    // Define a local variable to keep how big each step should be
+    var step = options.rotateStep;
+    // Define a local variable to keep the delay
+    var delay = options.rotateDelay;
+    // Create an interval
+    jObject.data('rotateInterval', setInterval(function() {
+      // Update the rotate angle
+      rotate = (rotate + step) % 360;
+      // Rotate the image
+      Rotate(jObject, rotate);
+    }, delay));
+  }
+  // A wrapper function to allow rotate on different browsers
+  function Rotate(jObject, rotate) {
+    // W3C
+    jObject.css('transform', 'rotate('+rotate+'deg)');
+  }
+  
+  // Extend jQuery(selector)
+  jQuery.fn.extend({
+    photoImages: function(options) {
+      // Fix all options
+      options = Optionize(options);
+      // Loop through all all matches from the given selector
+      return this.each(function() {
+        // Find the jQuery object for this item
+        var jThis = jQuery(this);
+        // Set the border
+        SetBorder(options, jThis);
+        // Set the margin
+        SetMargin(options, jThis);
+        // Check if the image should be rotated
+        if(options.rotate != 'leave') {
+          if(options.rotate == 'random') {
+            // Rotate the image by a random value
+            DoRotateRandom(options, jThis);
+          } else if(options.rotate == 'seed') {
+            // Rotate the image by a seeded value
+            DoRotateSeed(options, jThis);
+          } else if(options.rotate == 'animate') {
+            // Initiate an animation for the image
+            DoRotateAnimate(options, jThis);
+          } else {
+            // Rotate the image by a fixed value
+            Rotate(jThis, options.rotate);
+          }
+        }
+      });
+    }
+  });
+  
+}(jQuery));
